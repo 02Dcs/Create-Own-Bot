3da97b3921d4aa08721a4da0adaeb9c3fa6a79d2
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-11-20 18:01:21 +0100

    Update BotHandle.lua

--- BotHandle.lua
+++ BotHandle.lua
@@ -38,8 +38,10 @@ function PredictUser(NameVariable)
     for i,v in pairs (Table) do
         if string.find(string.lower(v.Name), string.lower(NameVariable)) then
 	      print(v.Name)
+	return v.Name
         elseif string.find(string.lower(v.DisplayName), string.lower(NameVariable)) then
              print(v.Name)
+	return v.Name
         end
     end
 end
