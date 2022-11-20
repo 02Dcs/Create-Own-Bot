9cc375f48d93201a063396b97e0c267b03a0a0f8
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-11-20 16:03:51 +0100

    Update BotHandle.lua

--- BotHandle.lua
+++ BotHandle.lua
@@ -29,10 +29,6 @@ function PredictUser(NameVariable)
     local Table = game.Players:GetChildren()
     for i,v in pairs (Table) do
         if string.find(string.lower(v.Name), string.lower(NameVariable)) then
-            return v.Name
-        elseif string.find(string.lower(v.DisplayName), string.lower(NameVariable)) then
-            return v.Name
-        end
     end
 end
 		
