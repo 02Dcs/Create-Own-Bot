95ad3ad366002c995d409532182d229645c978dc
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-11-20 15:59:31 +0100

    Update BotHandle.lua

--- BotHandle.lua
+++ BotHandle.lua
@@ -7,6 +7,17 @@ if syn then
 syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Create-Own-Bot/main/BotHandle.lua", true))()') 
 end
 
+function PredictUser(NameVariable)
+    local Table = game.Players:GetChildren()
+    for i,v in pairs (Table) do
+        if string.find(string.lower(v.Name), string.lower(NameVariable)) then
+            return v.Name
+        elseif string.find(string.lower(v.DisplayName), string.lower(NameVariable)) then
+            return v.Name
+        end
+    end
+end
+
 local msg
 local players, replicatedStorage = game:GetService("Players"), game:GetService("ReplicatedStorage");
 local defaultChatSystemChatEvents = replicatedStorage:FindFirstChild("DefaultChatSystemChatEvents");
@@ -25,17 +36,6 @@ if AddWhitelist.Whitelist == true then
 	end
 end
 		
-function PredictUser(NameVariable)
-    local Table = game.Players:GetChildren()
-    for i,v in pairs (Table) do
-        if string.find(string.lower(v.Name), string.lower(NameVariable)) then
-            return v.Name
-        elseif string.find(string.lower(v.DisplayName), string.lower(NameVariable)) then
-            return v.Name
-        end
-    end
-end
-
 function Msg(b)
 	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(b), "All")
 end
