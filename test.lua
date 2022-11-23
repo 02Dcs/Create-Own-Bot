339e85a83a5ff70485209f992fe4a147ea288c00
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-11-23 19:12:28 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,16 +8,15 @@ local prefix; local msg;
 local ChatBotApi = {}
 
 function PredictUser(NameVariable, ...)
-    local Table = game.Players:GetChildren()
-    for i,v in pairs (Table) do
-        if string.find(string.lower(v.Name), string.lower(NameVariable)) then
-	      return v.Name
-        elseif string.find(string.lower(v.DisplayName), string.lower(NameVariable)) then
-	     return v.Name
-        end
-    end
+local Table = game.Players:GetChildren()
+for i,v in pairs (Table) do
+if string.find(string.lower(v.Name), string.lower(NameVariable)) then
+return v.Name
+elseif string.find(string.lower(v.DisplayName), string.lower(NameVariable)) then
+return v.Name
 end
-
+end
+end
 
 function ChatBotApi.New(String,  ...)
 local properties = {}
