7802adf888fac72ffeff7e0ca99c50806672278a
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-07-02 23:11:50 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -3,31 +3,38 @@
 -- >> Handle 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/02Dcs/Create-Own-Bot/main/BotHandle.lua", true))()
 
--- >> Costum prefix
-Addprefix = '/'
+--[[
+everyone/all = All Users!
+whitelist/wht = Whitelist Users!
+player/plr = Find Player!
+variable/vr = Say The Index!
+]]
 
--- >> WhitelistSym
-AddWhitelist = {
-Whitelist = true,
-'PlayerNameHere',
-}
+-->> Notification
+notification(false);
+-- >> Costum prefix
+changeprefix('$');
 
 -- >> MsgChat
-addCommand("say", function()
-Msg("test")
+addcommand("chat", true, 'everyone', function()
+createmsg('test')
 end)
 
 -- >> addCommand
-addCommand("test", function()
-print("a")
+addcommand("command", true, 'everyone', function()
+print('hello!')
+end)
+
+-- >> Whitelist
+add('User1'); add('User2');
+
+addcommand('whitelist', false, 'whitelist', function()
+createmsg('hi whitelist user!')
 end)
 
 -- >> PredictUser
-addCommand("to", function() -- Command: test
-maxsplit = 2; -- Spilt The Msg
-local Args = PredictUser(tostring(string.split(msg," ")[maxsplit])); -- Handle
-print(Args) -- Prints player name!
+addcommand("predict", true, 'everyone', function() -- Handle
+createmsg('We found: ' .. addpredict('player', 2)) -- addpredict('variable', 2))
 end)
-
 
 -- More Coming Soon!
