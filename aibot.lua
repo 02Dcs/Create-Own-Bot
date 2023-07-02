fb5ba1af8c4e88fa78bdd31bb67ad13749beca78
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-07-03 01:15:57 +0200

    Update aibot.lua

--- aibot.lua
+++ aibot.lua
@@ -1,3 +1,4 @@
+local aibot = {}
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;
 
@@ -5,7 +6,7 @@ local rp = game:GetService('ReplicatedStorage')
 
 repeat game:IsLoaded() until task.wait(1)
 
-local function notification(e)	
+function notification(e)	
 	if e then
 		Notify({
 			Description = "Ai-Bot Control";
@@ -13,11 +14,11 @@ local function notification(e)
 			Duration = 5;
 		});
 	else 
-		return nil;
+		print('...')
 	end
 end
 
-local function createmsg(v)
+function createmsg(v)
 	pcall(function()
 		if not rp:FindFirstChild('DefaultChatSystemChatEvents') then
 			local g = game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(v)
@@ -33,16 +34,16 @@ local settingsl = {
 	wht = {},
 }; __index = settingsl;
 
-local function changeprefix(x)
-	return string.gsub(settingsl.prefix, settingsl.prefix, x);
+function changeprefix(x)
+	string.gsub(settingsl.prefix, settingsl.prefix, x);
 end
 
-local function add(b)
+function add(b)
 	table.insert(settingsl.wht, b); table.sort(settingsl.wht)
 end
 
 local predict
-local function addcommand(c, enable, user, callback, ...)
+function aibot:addcommand(c, enable, user, callback, ...)
 	for d, z in pairs(game.Players:GetChildren()) do
 		local callback = callback or function() end
 		local args = {...}
@@ -70,24 +71,4 @@ local function addcommand(c, enable, user, callback, ...)
 	end
 end
 
-local function b(x)
-	for x, j in pairs (game.Players:GetChildren()) do
-		if string.find(string.lower(x.Name), string.lower(x)) and string.find(string.lower(x.DisplayName) then
-			return x.Name
-		elseif string.find(string.lower(x.DisplayName), string.lower(x)) then
-			return x.Name
-		end
-	end
-end
-
-local function addpredict(ch, num)
-	if ch == 'variable' or ch == 'vr' then
-		return tostring(string.split(predict," ")[num]);
-	elseif ch == 'player' or ch == 'plr' then
-		return b(tostring(string.split(predict," ")[num]));
-	end
-end
-
-local function addplayer(num)
-	return tostring(string.split(predict," ")[num]);
-end
+return aibot
