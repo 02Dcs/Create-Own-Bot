b5c3947aad53c9c45bf4b0e71bbccce2da5eb35e
02Dcs <95237577+02Dcs@users.noreply.github.com>
2023-07-03 13:53:03 +0200

    Update and rename a.lua to ai.lua

---
+++ ai.lua
@@ -0,0 +1,11 @@
+
+return {
+	ai = function(Properties)
+	local Properties = typeof(Properties) == "table" and Properties or {};
+        function Properties:msg(x)
+         print(x);
+end
+	end,
+}
+
+
